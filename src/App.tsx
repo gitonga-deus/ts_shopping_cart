f0ff31ad25fc5f84abaf7b7af9692fd10b79bb16
gitonga <gitonga.deus@gmail.com>
2022-10-17 13:30:25 +0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+// Pages
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Store } from "./pages/Store"
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+// Bootstrap
+import { Container } from "react-bootstrap"
+
+const App = () => {
+	return (
+		<Container className="mb-4">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/store" element={<Store />} />
+			</Routes>
+		</Container>
+	)
 }
 
-export default App
+export default App;
