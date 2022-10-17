c18c3e1536f6e69e38097cbf5858d63d418ecbb7
gitonga <gitonga.deus@gmail.com>
2022-10-17 14:08:31 +0300

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { Store } from "./pages/Store"
 
+// Components
+import { Navbar } from "./components/Navbar"
+
 // Bootstrap
 import { Container } from "react-bootstrap"
 
 const App = () => {
 	return (
-		<Container className="mb-4">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/store" element={<Store />} />
-			</Routes>
-		</Container>
+		<>
+			<Navbar />
+			<Container className="mb-4">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/store" element={<Store />} />
+				</Routes>
+			</Container>
+		</>
 	)
 }
 
